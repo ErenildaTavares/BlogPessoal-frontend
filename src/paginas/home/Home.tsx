f270bc31fa5bf7e483ee42db122a445e79466d46
Erenilda Tavares <erenyldajsilva@gmail.com>
2023-05-11 18:05:10 -0300

    Iniciando blog pessoal com react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Home.css'
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://i.imgur.com/G0k9cIq.jpeg" alt="Imagem tela inicial astronauta" className="img" />
+        </>
+    );
+}
+export default Home;
